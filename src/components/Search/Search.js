5d382275e5a526aeea0689fb7061b272ee8587d9
Renat <renatsubzero10@gmail.com>
2026-04-15 10:52:50 +0300

    0.0.6

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,22 +1,29 @@
+import { Link, NavLink } from 'react-router-dom'; // 1. Импортируем Link и NavLink
 import './Search.css';
 
 function Search() {
   return (
     <div className='SearchPanel'>
-      <div className="Search">
-        <div className="Panel">
-          <h3>Пошук у Крамниці...</h3>
-          <img src='Icons.svg' alt='Icon' />
-        </div>
-        <div className="Selector">
-          <h3>Каталог</h3>
-          <h3>Новини</h3>
-        </div>
+      
+      <div className="Panel">
+        <input type="text" placeholder="Пошук у Крамниці..." className="SearchInput" />
+        <img src='Icons.svg' alt='Icon' className="SearchIcon" />
       </div>
-      <div className="Icons">
-        <img src='Wishlist.svg' alt='Wishlist' />
-        <img src='Cart.svg' alt='Cart' />
+
+      <div className="Selector">
+        <NavLink to="/catalog" className="SelectorLink">Каталог</NavLink>
+        <NavLink to="/news" className="SelectorLink">Новини</NavLink>
       </div>
+
+      <div className="IconsContainer">
+        <Link to="/wishlist" className='IconObj'>
+          <img src='Wishlist.svg' alt='Wishlist' />
+        </Link>
+        <Link to="/cart" className='IconObj'>
+          <img src='Cart.svg' alt='Cart' />
+        </Link>
+      </div>
+
     </div>
   );
 }
