e6ddbb51b66afcfbcff86b6bd94882d96e6c7808
Your Name <Your Name>
2026-05-05 11:25:36 +0300

    modal window

--- src/components/LibraryPage/LibraryPage.js
+++ src/components/LibraryPage/LibraryPage.js
@@ -6,15 +6,13 @@ import './LibraryPage.css';
 import React, { useState } from 'react';
 
 const pictures = [{ image: './pictures/1.png' }, { image: './pictures/2.png' }, { image: './pictures/3.png' }, { image: './pictures/4.png' },
-{ image: './pictures/5.png' }, { image: './pictures/6.png' }, { image: './pictures/7.png' }, { image: './pictures/8.png' }, { image: './pictures/9.png' },{ image: './pictures/10.png' },]
+{ image: './pictures/5.png' }, { image: './pictures/6.png' }, { image: './pictures/7.png' }, { image: './pictures/8.png' }, { image: './pictures/9.png' }, { image: './pictures/10.png' },]
 
 function LibraryPage() {
 
   const [isOpen, setIsOpen] = useState(false);
 
-  function addBlock() {
-    setIsOpen(true)
-  }
+  const toggleModal = () => setIsOpen(!isOpen);
 
   return (
     <div>
@@ -30,16 +28,66 @@ function LibraryPage() {
             <h2>Усі ігри</h2>
             <h2>Обране</h2>
             <h2>Моя колекція</h2>
-            <img src='./images/create.svg' onClick={addBlock}/>
+            <img src='./images/create.svg' onClick={toggleModal} />
           </div>
           <div className='games-block'>
-              {
-                pictures.map((picture) => (
-                  <img src={picture.image} className='games-block-img' alt="game"/>
-                ))
-              }
+            {
+              pictures.map((picture) => (
+                <img src={picture.image} className='games-block-img' alt="game" />
+              ))
+            }
           </div>
         </div>
+        
+        {isOpen && (
+          <div className='modal-overlay' onClick={toggleModal}>
+            <div className='modal-content' onClick={(e) => e.stopPropagation()}>
+              <div className='modal-header'>
+                <h2>Нова колекція</h2>
+                <span className='close-btn' onClick={toggleModal}>&times;</span>
+              </div>
+
+              <div className='modal-body'>
+                <div className='input-group'>
+                  <label>Назва колекції <span>0/25</span></label>
+                  <input type="text" placeholder="Придумайте назву колекції..." />
+                </div>
+
+                <div className='input-group'>
+                  <label>Додайте ігри</label>
+                  <input type="text" placeholder="Пошук ігор за назвою..." />
+                </div>
+
+                <h2 className='other-text'>Ви можете додати ігри до колекції пізніше</h2>
+
+                <div className='container-option-block'>
+                  <div className='option-block'>
+                    <h3>Название игры</h3>
+                    <span>&times;</span>
+                  </div>
+                  <div className='option-block'>
+                    <h3>Название игры</h3>
+                    <span>&times;</span>
+                  </div>
+                  <div className='option-block'>
+                    <h3>Игра</h3>
+                    <span>&times;</span>
+                  </div>
+                  <div className='option-block'>
+                    <h3>Название игры</h3>
+                    <span>&times;</span>
+                  </div>
+                  <div className='option-block'>
+                    <h3>Длинное название игры</h3>
+                    <span>&times;</span>
+                  </div>
+                </div>
+
+                <button className='create-btn'>Створити</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div >
   );
