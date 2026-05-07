48014a738d3d4e1ff1b53ebd675662244c83727a
Your Name <Your Name>
2026-05-07 13:39:17 +0300

    07

---
+++ src/components/LibraryGame/LibraryGame.js
@@ -0,0 +1,15 @@
+import { SideBar } from '../SideBar/SideBar';
+import './LibraryGame.css';
+
+function LibraryPage() {
+  return (
+    <div>
+      <div className='block-lib'>
+        <SideBar/>
+        <div className='block-lib-right'>
+
+        </div>
+      </div>
+    </div >
+  )
+}
