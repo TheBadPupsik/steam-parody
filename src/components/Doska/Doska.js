71e995fb81c754b5d393112f4e5c6bf5469d2489
Glebchik39 <lubimovanatalia06@gmail.com>
2026-05-08 18:23:10 +0300

    update

--- src/components/Doska/Doska.js
+++ src/components/Doska/Doska.js
@@ -14,37 +14,34 @@ export function Doska() {
           <div className='tema'>
             <div className='tema_nazvanie'>
               <img src='/pictures/Icons.png' alt='' />
-              <p>Темна Тема</p>
+              <p className='p_doska'>Темна Тема</p>
             </div>
-            <label class="switch">
-            <input type="checkbox"/>
-            <span class="slider"></span>
-          </label>          
+            <img src='/pictures/Toggle.png'></img>        
           </div>
 
           <NavLink to='/general' className='settings'>
             <img src='/pictures/Icons (1).png' className='icon_settings' alt='' />
-            <p>Загальні налаштування</p>
+            <p className='p_doska'> Загальні налаштування</p>
           </NavLink>
 
           <NavLink to='/password' className='settings'>
             <img src='/pictures/Icons (2).png' className='icon_settings' alt='' />
-            <p>Пароль</p>
+            <p className='p_doska'>Пароль</p>
           </NavLink>
 
           <NavLink to='/notifications' className='settings'>
             <img src='/pictures/Icons (3).png' className='icon_settings' alt='' />
-            <p>Сповіщення</p>
+            <p className='p_doska'>Сповіщення</p>
           </NavLink>
 
           <NavLink to='/wallet' className='settings'>
             <img src='/pictures/Icons (4).png' className='icon_settings' alt='' />
-            <p>Гаманець</p>
+            <p className='p_doska'>Гаманець</p>
           </NavLink>
 
           <NavLink to='/deleteAccount' className='settings'> 
            <img src='/pictures/Icons (5).png' className='icon_settings' alt='' />
-            <p>Видалення акаунта</p>
+            <p className='p_doska'>Видалення акаунта</p>
           </NavLink>
 
         </div>
