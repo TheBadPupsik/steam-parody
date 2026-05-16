e81aa4ec9e5950c81c98c0420e9a706dc00e5c7a
Renat <renatsubzero10@gmail.com>
2026-05-16 17:44:45 +0300

    ProfileUpdate

--- src/components/ProfileBar/ProfileBar.js
+++ src/components/ProfileBar/ProfileBar.js
@@ -4,15 +4,34 @@ function ProfileBar() {
   return (
     <div className="ProfileBar">
       <div className='CategoryProfile'>
-        <span className='ProfileBarBTN'>Головна</span>
-        <span>Значки</span>
-        <span>Ігри</span>
-        <span>Бажане</span>
-        <span>Обговорення</span>
-        <span>Скрішоти</span>
-        <span>Відео</span>
-        <span>Гайди</span>
-        <span>Рецензії</span>
+        <div className='ProfileLVL'>
+          <h2>Рівень</h2>
+          <img src='././images/Level Badges.svg' />
+        </div>
+        <NavLink to='/profile' className='ProfileBarBTN'>Головна<span>100</span> </NavLink>
+        <NavLink to='/profile/badges' className='ProfileBarBTN'>Значки<span>100</span> </NavLink>
+        <NavLink to='/profile/games' className='ProfileBarBTN'>Ігри<span>100</span> </NavLink>
+        <NavLink to='/profile/wishlist' className='ProfileBarBTN'>Бажане<span>100</span> </NavLink>
+        <NavLink to='/profile/discussion' className='ProfileBarBTN'>Обговорення<span>100</span> </NavLink>
+        <NavLink to='/profile/screenshot' className='ProfileBarBTN'>Скріншоти<span>100</span> </NavLink>
+        <NavLink to='/profile/videos' className='ProfileBarBTN'>Відео<span>100</span> </NavLink>
+        <NavLink to='/profile/guide' className='ProfileBarBTN'>Гайди<span>100</span> </NavLink>
+        <NavLink to='/profile/review' className='ProfileBarBTN'>Рецензії<span>100</span> </NavLink>
+      </div>
+
+      <div className='ProfileFriendsList'>
+        <div className='FriendList'>
+          <NavLink to='/profile/' className='ProfileBarBTN'>Друзі<span>25</span> </NavLink>
+        </div>
+
+        <div className='FriendsList'>
+          <span className='Friend'> <span><img src='././images/ProfileFriendPicture.png' alt='ProfileAva' /> <h2>GhostRogue</h2></span><img src='././images/Level Badges.svg' /></span>
+          <span className='Friend'> <span><img src='././images/ProfileFriendPicture.png' alt='ProfileAva' /> <h2>s1imerock</h2></span><img src='././images/Level Badges.svg' /></span>
+          <span className='Friend'> <span><img src='././images/ProfileFriendPicture.png' alt='ProfileAva' /> <h2>NikaNii</h2></span><img src='././images/Level Badges.svg' /></span>
+          <span className='Friend'> <span><img src='././images/ProfileFriendPicture.png' alt='ProfileAva' /> <h2>whysxugly</h2></span><img src='././images/Level Badges.svg' /></span>
+          <span className='Friend'> <span><img src='././images/ProfileFriendPicture.png' alt='ProfileAva' /> <h2>zuzeyka</h2></span><img src='././images/Level Badges.svg' /></span>
+
+        </div>
       </div>
 
     </div>
--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -4,22 +4,26 @@ import './ProfilePage.css';
 function ProfilePage() {
   return (
     <div className='ProfilePage'>
+
       <div className='BannerWrapper'>
         <img className='Banner' src='././images/BannerImg.png' alt='BannerIMG' />
         <img className='IconProfile' src='./images/profile-avatar.jpg' alt='avatar' />
-        <div>
-          <h2 className='Nickname'>Gaymer Master</h2>
-          <p className='StatusOnline'>Онлайн</p>
 
-          <span className='ProfileBTN'>
-            <img src='././images/pencil.svg' alt='pencil' />
-            <p>Редагувати профіль</p>
-          </span>
-        </div>
+              <div className='ProfileData'>
+        <h2 className='Nickname'>Gaymer Master</h2>
+        <p className='StatusOnline'>Онлайн</p>
 
-        <p className='Describe'>У пошуках нових пригод! Кожен новий рівень – це можливість пережити незабутні моменти та здобути новий досвід.</p>
+        <span className='ProfileBTN'>
+          <img src='././images/pencil.svg' alt='pencil' />
+          <p>Редагувати профіль</p>
+        </span>
       </div>
-      <ProfileBar></ProfileBar>
+      </div>
+
+
+
+      <ProfileBar />
+
     </div>
   );
 }
